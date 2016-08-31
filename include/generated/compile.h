470af164dd9300fa3d6d6affdb9e19edaf2022e7
wang <1179952540@qq.com>
2016-08-31 16:34:33 +0800

    add gitignore to the git repositories

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 45 */
+/* This file is auto generated, version 46 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#45 SMP PREEMPT Wed Aug 31 09:44:05 CST 2016"
+#define UTS_VERSION "#46 SMP PREEMPT Wed Aug 31 14:40:50 CST 2016"
 #define LINUX_COMPILE_BY "alex"
 #define LINUX_COMPILE_HOST "Alex"
 #define LINUX_COMPILER "gcc version 4.6.2 20110630 (prerelease) (Freescale MAD -- Linaro 2011.07 -- Built at 2011/08/10 09:20) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.1.15"
+#define UTS_RELEASE "4.1.15-g8b9ab81-dirty"
