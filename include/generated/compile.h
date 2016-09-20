51556472d234443f31d4920e46d1b117efe7e555
wang <1179952540@qq.com>
2016-09-20 09:06:41 +0800

    make on the new vitual machine

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 53 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#53 SMP PREEMPT Tue Sep 13 18:05:58 CST 2016"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Sep 19 16:45:51 CST 2016"
 #define LINUX_COMPILE_BY "alex"
 #define LINUX_COMPILE_HOST "Alex"
 #define LINUX_COMPILER "gcc version 4.6.2 20110630 (prerelease) (Freescale MAD -- Linaro 2011.07 -- Built at 2011/08/10 09:20) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.1.15-ged3bd72"
+#define UTS_RELEASE "4.1.15-g82dba87-dirty"
